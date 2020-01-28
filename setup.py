3c1bec9d8793b06df7ddbf80e6ca83eda9d008a9
Vemund H. Aarstrand <vemundaa@gmail.com>
2020-01-28 15:31:57 +0100

    Update publication details

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name='mjooln',
+  packages=['mjooln'],
+  version='0.1',
+  license='MIT',
+  description='Environmentally Friendly File Handling',
+  author='Vemund Halmø Aarstrand',
+  author_email='vemundaa@gmail.com',
+  url='https://github.com/vemundaa/mjooln',
+  download_url='https://github.com/vemundaa/mjooln/archive/v_01.tar.gz',
+  keywords=['os', 'path', 'file', 'folder', 'file handling'],
+  install_requires=[
+      'pytest',
+      'python-dateutil',
+      'pytz',
+      'simplejson',
+      'psutil',
+      'cryptography',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
