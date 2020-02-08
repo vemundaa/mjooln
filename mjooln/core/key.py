583d70537f9c46352740e6b6a01c658804db8aad
Vemund H. Aarstrand <vemundaa@gmail.com>
2020-02-08 09:05:22 +0100

    Fix error message

--- mjooln/core/key.py
+++ mjooln/core/key.py
@@ -9,8 +9,8 @@ class Key(str):
     ALLOWED_CHARACTERS = string.ascii_lowercase + string.digits + '_'
     ALLOWED_STARTSWITH = string.ascii_lowercase
     MINIMUM_ALLOWED_LENGTH = 3
-    RESERVED = '___'
-    SEPARATOR = '__'
+    RESERVED = '___'  # Used for separating key in segment
+    SEPARATOR = '__'  # Separates groups in key
 
     def __new__(cls, key):
         if isinstance(key, Key):
@@ -20,12 +20,12 @@ class Key(str):
                                  f'while minimum length is {cls.MINIMUM_ALLOWED_LENGTH}')
         if not key[0] in cls.ALLOWED_STARTSWITH:
             raise KeyFormatError(f'Invalid startswith. '
-                                 f'Key \'{key}\' cannot start with \'{key[0]}\'.'
-                                 f'Allowed characters are: {cls.ALLOWED_STARTSWITH}')
+                                 f'Key \'{key}\' cannot start with \'{key[0]}\'. '
+                                 f'Allowed startswith characters are: {cls.ALLOWED_STARTSWITH}')
         invalid_characters = [x for x in key if x not in cls.ALLOWED_CHARACTERS]
         if len(invalid_characters) > 0:
             raise KeyFormatError(f'Invalid character(s). '
-                                 f'Key \'{key}\' cannot contain any of \'{invalid_characters}\'.'
+                                 f'Key \'{key}\' cannot contain any of {invalid_characters}. '
                                  f'Allowed characters are: {cls.ALLOWED_CHARACTERS}')
         if cls.RESERVED in key:
             raise KeyFormatError(f'Key contains reserved element. '
@@ -35,6 +35,9 @@ class Key(str):
 
     def parts(self):
         return self.split(self.SEPARATOR)
+
+    def with_separator(self, separator):
+        return separator.join(self.parts())
 
     @classmethod
     def elf(cls, key):
