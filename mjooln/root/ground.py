db9d7e05294f664e085e07380bffb4117fa1bd13
Vemund H. Aarstrand <vemundaa@gmail.com>
2020-06-02 15:13:16 +0200

    Rewrite ground to inherit root

--- mjooln/root/ground.py
+++ mjooln/root/ground.py
@@ -1,94 +1,61 @@
-from mjooln import Root, Dic, Folder, File, RootError, Zulu
+from mjooln import Root, Dic, Folder, File, RootError, Zulu, NotRootException
 
-# TODO: A field can have split trees (identical, indexed, and with randomized where each leaf is)
+# TODO: A field can have split trees (identical,
+#  indexed, and with randomized where each leaf is)
 
 
-class GroundProblem(Exception):
+class GroundProblem(RootError):
     pass
 
 
-class Ground(Folder):
-    """Folder wrapper with the ability to list roots within"""
-    FILE_NAME = f'{File.HIDDEN_STARTSWITH}ground' \
-                f'{File.EXTENSION_SEPARATOR}{File.JSON_EXTENSION}'
+class NotGroundException(NotRootException):
+    pass
 
-    @classmethod
-    def search_for(cls):
-        files = File.list(pattern=cls.FILE_NAME, recursive=True)
-        return [cls(x.folder()) for x in files]
 
-    @classmethod
-    def _file(cls, folder):
-        return File.join(folder, cls.FILE_NAME)
+class Ground(Root):
+
+    HOME_GROUND = 'ground_at_home'
+    GROUND = 'ground'
+    SPECIES = GROUND
 
     @classmethod
     def is_ground(cls, folder):
-        if not folder.exists() or not cls._file(folder).exists():
-            return False
-        else:
+        try:
+            cls(folder)
             return True
+        except NotRootException:
+            return False
 
     @classmethod
-    def settle(cls, folder, given_name='nn', **kwargs):
-        if not folder.exists():
-            raise GroundProblem(f'Cannot settle if the folder '
-                                f'does not exist: {folder}')
-        file = cls._file(folder)
-        if file.exists():
-            dic = file.read()
-            if 'given_name' in dic:
-                name_ = dic['given_name']
-            else:
-                name_ = 'nn'
-            raise GroundProblem(f'This folder is already '
-                                f'occupied by \'{name_}\'.')
-        else:
-            dic = {
-                'given_name': given_name
-            }
-            if kwargs:
-                dic.update(kwargs)
-            file.write(dic)
-            return cls(folder)
+    def plant(cls, folder, key, **kwargs):
+        raise GroundProblem('Cannot plant ground. Use settle()')
 
     @classmethod
-    def settle_at_home(cls, name='unknown'):
+    def uproot(self, with_force=False, key=None):
+        raise GroundProblem('Cannot uproot ground. Use unsettle()')
+
+    @classmethod
+    def settle(cls, folder, key, name='nn', **kwargs):
+        return super(Ground, cls).plant(folder=folder,
+                                        key=key,
+                                        name=name,
+                                        **kwargs)
+
+    @classmethod
+    def settle_at_home(cls):
+        # TODO: Consider the value of this.
         folder = Folder.home()
-        return cls.settle(folder, name=name)
-
-    def __str__(self):
-        return f'ground@{self.name()}'
+        return cls.settle(folder,
+                          key=cls.HOME_GROUND,
+                          name=cls.HOME_GROUND)
 
     def __init__(self, folder):
-        super().__init__()
-        if not self.is_ground(folder):
-            raise GroundProblem(f'Folder is not settled ground: {folder}')
+        self.name = 'nn'
+        super(Ground, self).__init__(folder)
 
-    def bury(self, dic):
-        # TODO: Refactor. At the moment it is something resembling a log.
-        file = self._file(self)
-        buried_dic = file.read()
-        buried_dic[str(Zulu())] = dic
-        file.write(buried_dic)
+    def __str__(self):
+        return f'{self.name}@{self.folder()}'
 
-    def retrieve(self):
-        file = self._file(self)
-        return file.read()
+    def unsettle(self, with_force=False, key=None):
+        super(Ground, self).uproot(with_force=with_force, key=key)
 
-    def unsettle(self):
-        self._file(self).delete()
-
-    def root(self, key):
-        return Root(self.append(key))
-
-    def roots(self):
-        paths = self.list()
-        folders = [x for x in paths if x.is_folder()]
-        roots = []
-        for folder in folders:
-            try:
-                root = Root(folder)
-                roots.append(root)
-            except RootError:
-                pass
-        return roots
