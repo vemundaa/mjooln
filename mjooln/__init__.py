b675873fdb4078ea3026faa0db5073a7d997289e
Vemund H. Aarstrand <vemundaa@gmail.com>
2020-06-10 11:31:28 +0200

    Update init with ground

--- mjooln/__init__.py
+++ mjooln/__init__.py
@@ -12,18 +12,4 @@ from .core import File, FileError
 from .tree import Root, RootError, NotRootException
 from .tree import Leaf, LeafError
 from .tree import Tree, TreeError
-
-# from mjooln.atom.identity import Identity, IdentityError
-# from mjooln.atom.key import Key, KeyFormatError
-# from mjooln.atom.zulu import Zulu, ZuluError
-# from mjooln.atom.atom import Atom, AtomError
-#
-# from mjooln.core.dic_doc import Dic, Doc, JSON
-# from mjooln.core.crypt import Crypt, CryptError
-# from mjooln.core.path import Path, PathError
-# from mjooln.core.folder import Folder, FolderError
-# from mjooln.core.file import File, FileError
-#
-# from mjooln.tree.root import Root, RootError, NotRootException
-# from mjooln.tree.leaf import Leaf, LeafError
-# from mjooln.tree.tree import Tree, TreeError
+from .tree import Ground, GroundProblem, NoGround
--- mjooln/tree/__init__.py
+++ mjooln/tree/__init__.py
@@ -1,3 +1,4 @@
 from .root import Root, RootError, NotRootException
 from .tree import Tree, TreeError, NotTreeException
 from .leaf import Leaf, LeafError
+from .ground import Ground, GroundProblem, NoGround
